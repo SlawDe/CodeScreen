1e3ffc5dcfe6c38e15548cf6b151b959d8c87d85
SlawDe <33845097+SlawDe@users.noreply.github.com>
2022-09-26 07:33:54 +0200

    Add files via upload

---
+++ car.js
@@ -0,0 +1,36 @@
+    /**
+     * This is the function where you need to write your code solution
+     * "roadDamages" is defined down below, where the function gets called
+     */
+    function moveCar(roadDamages) {
+       // TODO Implement the solution here
+	for(int i = 0 ; i > roadDamages.length ; i++){
+	    int j=0;
+        if roadDamages[i] = 0{
+        count >> "The car goes to step ${i + 1}";
+        else
+        count >> "The car is damaged";
+        j++;
+            }
+        if (j==2){
+        }
+        count >> "The car is broken" ;
+        }
+    }
+
+
+    /**
+    - for the first time, it has to write in console "The car is damaged", but it can continue to go to the next step
+    - for the second time, it has to write in console "The car is broken" and it can not continue to drive anymore
+
+     * Function gets called
+     * You can call this function with different set of array values in order to test different scenarios
+     * Do not change the amount of values inside the array
+     */
+    moveCar([false, false, false, false, true, false, true, false, false, false]);
+
+    /**
+     * This code line is required in order to trigger automated tests
+     * Do not change this code line
+     */
+    export default moveCar;
